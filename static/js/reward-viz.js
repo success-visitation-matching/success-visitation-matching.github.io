33138cdc26fea2136ca15cedce47e62abe626f55
Raymond <raymond1123581321@gmail.com>
2026-06-20 11:44:56 -0700

    Added reward visualization

---
+++ static/js/reward-viz.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  var checkpoints = [0, 6300, 6600, 6900, 7200, 7500, 7800, 8100, 8400, 8700, 9000, 9300, 9600, 9900];
+  var $slider = $('#reward-viz-slider');
+  var $frames = $('.reward-viz-frame');
+  var $step = $('#reward-viz-step');
+
+  if (!$slider.length) {
+    return;
+  }
+
+  $slider.attr('max', checkpoints.length - 1);
+
+  function updateRewardViz(index) {
+    var ck = checkpoints[index];
+    $frames.each(function() {
+      var task = $(this).data('task');
+      $(this).attr('src', './static/images/task' + task + '/reward_frames/ck_' + ck + '.png');
+    });
+    $step.text('Training step: ' + ck.toLocaleString());
+  }
+
+  $slider.on('input', function() {
+    updateRewardViz(parseInt(this.value, 10));
+  });
+
+  updateRewardViz(0);
+});
